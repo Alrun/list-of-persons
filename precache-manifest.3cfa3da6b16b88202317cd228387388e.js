8fd4b163e136416502e43b65ac338083a6374f1c
Alrun-PC <fkhey2016@yandex.ru>
2019-10-09 21:29:34 +0300

    Updates

---
+++ precache-manifest.3cfa3da6b16b88202317cd228387388e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cdcabe00c287baff4fcaeb0932adb6",
+    "url": "/pl/index.html"
+  },
+  {
+    "revision": "8fd01b9e21e7f5d2ce07",
+    "url": "/pl/static/js/2.f202ee3e.chunk.js"
+  },
+  {
+    "revision": "5f0459974082115ebc9c",
+    "url": "/pl/static/js/main.40fba4ec.chunk.js"
+  },
+  {
+    "revision": "b8b4ad359044e9490f96",
+    "url": "/pl/static/js/runtime-main.69dab2c5.js"
+  }
+]);
